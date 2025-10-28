b87dfc913dada06a51b988aea3ff091c902882ed
DYORAAA <dyora4@gmail.com>
2025-10-28 11:32:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementByld('sapaButton');
-tombolSapa.addEventListener('click',fuction() {
-                            alert('Halo! Terimakasih sudah berkunjung!);
-                                  });
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', fuction() {
+      alert('Halo! Terimakasih sudah berkunjung!);
+});
